b2fe499235badb2a847d5fdc928cb756f212511d
Galiant <antonijodev@gmail.com>
2020-09-12 20:32:41 +0100

    Adding bootstrap to project

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,1 +1,12 @@
-export default () => <div>Welcome to next.js</div>;
+import { Button, Alert } from "reactstrap";
+
+export default () => {
+  return (
+    <div>
+      <div>
+        <Alert color="primary">Hello Foody project!</Alert>
+        &nbsp; <Button color="primary">Hello from next.js</Button>
+      </div>
+    </div>
+  );
+};
