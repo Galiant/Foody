78c9fb0e1576433a77fec2b69be111ed362f7de5
Galiant <antonijodev@gmail.com>
2020-10-17 20:31:26 +0100

    Setuping cookie with js-cookie

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import Head from "next/head";
 import Link from "next/link";
-
 import { Container, Nav, NavItem } from "reactstrap";
+import { logout } from "../lib/auth";
+import AppContext from "../context/AppContext";
 
-export default function Layout(props) {
-  const title = "Welcome to Next.js";
+const Layout = props => {
+  const title = "Welcome to Nextjs";
+  const { user, setUser } = useContext(AppContext);
+
   return (
     <div>
       <Head>
         <title>{title}</title>
-        <meta charSet="utf-s" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link
           rel="stylesheet"
@@ -26,6 +29,10 @@ export default function Layout(props) {
             a {
               color: white;
             }
+            h5 {
+              color: white;
+              padding-top: 11px;
+            }
           `}
         </style>
         <Nav className="navbar navbar-dark bg-dark">
@@ -36,19 +43,38 @@ export default function Layout(props) {
           </NavItem>
 
           <NavItem className="ml-auto">
-            <Link href="/login">
-              <a className="nav-link">Sign In</a>
-            </Link>
+            {user ? (
+              <h5>{user.username}</h5>
+            ) : (
+              <Link href="/register">
+                <a className="nav-link"> Sign up</a>
+              </Link>
+            )}
           </NavItem>
-
           <NavItem>
-            <Link href="/register">
-              <a className="nav-link"> Sign Up</a>
-            </Link>
+            {user ? (
+              <Link href="/">
+                <a
+                  className="nav-link"
+                  onClick={() => {
+                    logout();
+                    setUser(null);
+                  }}
+                >
+                  Logout
+                </a>
+              </Link>
+            ) : (
+              <Link href="/login">
+                <a className="nav-link">Sign in</a>
+              </Link>
+            )}
           </NavItem>
         </Nav>
       </header>
       <Container>{props.children}</Container>
     </div>
   );
-}
+};
+
+export default Layout;
